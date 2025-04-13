95e5e36b08c3d53fdee0d326c8d9fd8cdbfec0c5
Ghifaryh <mhghifaryy@gmail.com>
2025-04-13 12:29:33 +0700

    feat: add PostCard component and integrate it into the home page
    The PostCard component is introduced to display individual post details. The home page is updated to fetch and render a list of posts sorted by the newest first, utilizing the PostCard component for consistent post representation.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,21 @@
-export default function Home() {
-	return (
-		<div>
-			<h1>Home page</h1>
-		</div>
-	)
+import PostCard from "@/components/postCard"
+import { getCollection } from "@/lib/db"
+
+export default async function Home() {
+	const postsCollection = await getCollection("posts")
+	const posts = await postsCollection?.find().sort({ $natural: -1 }).toArray() // sort by newest
+
+	if (posts) {
+		return (
+			<div className="grid grid-cols-2 gap-6">
+				{posts.map((post) => (
+					<div key={post._id}>
+						<PostCard post={post} />
+					</div>
+				))}
+			</div>
+		)
+	} else {
+		return <p>Failed to fetch the data from the db</p>
+	}
 }
