32833480659d3cc951545d3c18e77100dfc8efa6
Ghifaryh <mhghifaryy@gmail.com>
2025-04-13 12:54:59 +0700

    feat: add loading component and suspense fallback for posts list
    To improve user experience, a loading component has been introduced to handle loading states. Additionally, the PostsList component is now wrapped in a Suspense boundary to prevent long loading times in case of database errors. This ensures a smoother user experience during data fetching.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,20 @@
 import PostCard from "@/components/postCard"
 import { getCollection } from "@/lib/db"
+import { Suspense } from "react"
 
 export default async function Home() {
+	return (
+		//  to prevent too long loading when the db is error
+		// <Suspense fallback={<p>Loading...</p>}>
+		// <PostsList />
+		// </Suspense>
+		// either using Suspense or loading.jsx, but not both at the same time
+
+		<PostsList />
+	)
+}
+
+async function PostsList() {
 	const postsCollection = await getCollection("posts")
 	const posts = await postsCollection?.find().sort({ $natural: -1 }).toArray() // sort by newest
 
